4916af832e2226418e99b0cf367d5b7a0bc7e474
Solon Galdino <solonfisicaufpb@gmail.com>
2023-04-23 03:16:23 -0300

    feat: adicioando cliente de mensageria google cloud tasks

--- api-1/src/modules/brands/useCases/RegisterBrands/implementations/RegisterBrandsUseCase.ts
+++ api-1/src/modules/brands/useCases/RegisterBrands/implementations/RegisterBrandsUseCase.ts
@@ -1,11 +1,12 @@
 import { FipeHttpClientInterface } from '@shared/httpClients/interfaces';
-import { ApiError } from '@shared/utils';
+import { ApiError, EnvVariables } from '@shared/utils';
 import { inject, injectable } from 'tsyringe';
 import {
   IRegisterBrandsUseCase,
   IRegisterBrandsUseCaseDTO,
   IRegisterBrandsUseCaseResponseDTO,
 } from '../interfaces';
+import { v2beta3 } from '@google-cloud/tasks';
 
 @injectable()
 export default class RegisterBrandsUseCase implements IRegisterBrandsUseCase {
@@ -19,8 +20,39 @@ export default class RegisterBrandsUseCase implements IRegisterBrandsUseCase {
 
     if (!lista) throw new ApiError(404, 'List not found');
 
+    const config = {
+      project: EnvVariables.get_GCP_PROJECT_ID(),
+      location: EnvVariables.get_GCP_LOCATION(),
+      queue: EnvVariables.get_GCP_QUEUE_NAME(),
+      url: EnvVariables.get_GCP_FUNCTION_URL(),
+      email: EnvVariables.get_GCP_EMAIL(),
+    };
+
+    const client = new v2beta3.CloudTasksClient();
+
+    const parent = client.queuePath(config.project, config.location, config.queue);
+
     for (const item of lista) {
-      console.log(item);
+      const convertedPayload = JSON.stringify(item);
+      const body = Buffer.from(convertedPayload).toString('base64');
+
+      client.createTask({
+        parent,
+        task: {
+          httpRequest: {
+            httpMethod: 'POST',
+            url: config.url,
+            oidcToken: {
+              serviceAccountEmail: config.email,
+              audience: new URL(config.url).origin,
+            },
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body,
+          },
+        },
+      });
     }
   }
 }
--- api-1/src/shared/utils/EnvVariables.ts
+++ api-1/src/shared/utils/EnvVariables.ts
@@ -3,11 +3,46 @@ import { env } from 'process';
 class EnvVariables {
   allEnvVariablesExist() {
     this.get_SERVER_PORT();
+    this.get_GCP_PROJECT_ID();
+    this.get_GCP_LOCATION();
+    this.get_GCP_QUEUE_NAME();
+    this.get_GCP_FUNCTION_URL();
+    this.get_GCP_EMAIL();
   }
 
   get_SERVER_PORT() {
     const envVariable = env.SERVER_PORT;
     if (!envVariable) throw new Error('Env variable SERVER_PORT not found.');
+    return envVariable;
+  }
+
+  get_GCP_PROJECT_ID() {
+    const envVariable = env.GCP_PROJECT_ID;
+    if (!envVariable) throw new Error('Env variable GCP_PROJECT_ID not found.');
+    return envVariable;
+  }
+
+  get_GCP_LOCATION() {
+    const envVariable = env.GCP_LOCATION;
+    if (!envVariable) throw new Error('Env variable GCP_LOCATION not found.');
+    return envVariable;
+  }
+
+  get_GCP_QUEUE_NAME() {
+    const envVariable = env.GCP_QUEUE_NAME;
+    if (!envVariable) throw new Error('Env variable GCP_QUEUE_NAME not found.');
+    return envVariable;
+  }
+
+  get_GCP_FUNCTION_URL() {
+    const envVariable = env.GCP_FUNCTION_URL;
+    if (!envVariable) throw new Error('Env variable GCP_FUNCTION_URL not found.');
+    return envVariable;
+  }
+
+  get_GCP_EMAIL() {
+    const envVariable = env.GCP_EMAIL;
+    if (!envVariable) throw new Error('Env variable GCP_EMAIL not found.');
     return envVariable;
   }
 }
